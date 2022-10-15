0fb531e881a6909a7999b4d3f538ef7858b3a517
zaval <antonzavalniuk@gmail.com>
2022-10-15 20:55:15 +0200

    make input controlable, onSendMessage function

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,15 +1,35 @@
 import { View, TextInput } from "react-native";
+import { useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 
 const InputBox = () => {
+  const [newMessage, setNewMessage] = useState("");
+
+  const onSendMesage = () => {
+    console.warn("Send a new message");
+
+    setNewMessage("");
+  };
+
   return (
     <View style={styles.container}>
       <AntDesign name="plus" size={24} color="royalblue" />
-      <TextInput style={styles.input} />
-      <MaterialIcons style={styles.send} name="send" size={24} color="white" />
+      <TextInput
+        value={newMessage}
+        onChangeText={setNewMessage}
+        multiline
+        style={styles.input}
+      />
+      <MaterialIcons
+        name="send"
+        size={24}
+        color="white"
+        style={styles.send}
+        onPress={onSendMesage}
+      />
     </View>
   );
 };
