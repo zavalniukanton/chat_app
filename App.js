b8b8eb147aec4b48aa58af7b9c7355ca2a30de3c
zaval <antonzavalniuk@gmail.com>
2022-10-15 20:12:11 +0200

    Chats screen and chat list

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import ChatListItem from "./src/components/ChatListItem";
+
+import ChatsScreen from "./src/screens/ChatsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem />
+      <ChatsScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
   },
 });
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,22 +1,24 @@
 import { View, Text, Image } from "react-native";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 import { styles } from "./styles";
 
-const ChatListItem = () => {
+const ChatListItem = ({ chat }) => {
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: "https://i.ibb.co/DKMJJQT/Steve.jpg" }}
-        style={styles.avatar}
-      />
+      <Image source={{ uri: chat.user.image }} style={styles.avatar} />
 
       <View style={styles.content}>
         <View style={styles.header}>
-          <Text style={styles.name}>Anton</Text>
-          <Text style={styles.text}>12:00</Text>
+          <Text style={styles.name}>{chat.user.name}</Text>
+          <Text style={styles.text}>
+            {dayjs(chat.lastMessage.createdAt).fromNow()}
+          </Text>
         </View>
 
-        <Text style={styles.text}>How are you?</Text>
+        <Text style={styles.text}>{chat.lastMessage.text}</Text>
       </View>
     </View>
   );
